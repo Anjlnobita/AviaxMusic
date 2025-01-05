b93dc49366c647da665545fe694ae5b6410ae4a3
Anjlnobita <jiteshkumar7200@gmail.com>
2025-01-05 16:27:53 +0530

    Update start.py

--- AviaxMusic/utils/inline/start.py
+++ AviaxMusic/utils/inline/start.py
@@ -25,10 +25,12 @@ def private_panel(_):
                 url=f"https://t.me/{app.username}?startgroup=true",
             )
         ],
-        [InlineKeyboardButton(text=_["S_B_4"], callback_data="settings_back_helper")],
+        [InlineKeyboardButton(text=_["S_B_4"], callback_data="settings_back_helper")
+      ],
         [
-                 InlineKeyboardButton(text=_["S_B_2"], url=config.SUPPORT_GROUP),
-        [
+
+            InlineKeyboardButton(text=_["S_B_2"], url=config.SUPPORT_CHAT),
+
             InlineKeyboardButton(text=_["S_B_6"], url=config.SUPPORT_CHANNEL),
         ],
     ]
