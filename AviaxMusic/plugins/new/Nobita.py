e1f0d595c80cc6fd1da660152ed4a0606e181baf
Anjlnobita <jiteshkumar7200@gmail.com>
2025-01-07 15:24:36 +0530

    Update new

---
+++ AviaxMusic/plugins/new/Nobita.py
@@ -0,0 +1,46 @@
+from difflib import SequenceMatcher
+import re
+import random
+from pyrogram import Client, filters
+from AviaxMusic import app
+from config import OWNER_ID
+from AviaxMusic.core.mongo import mongodb
+
+nobita = mongodb.nobi
+
+
+
+@app.on_message(
+    filters.command("tsave")
+    & filters.user(OWNER_ID)
+)
+async def tsave(client, message):
+    # Save data to MongoDB
+    text = message.text.split(" ", 1)[1]
+    usertext, bottexts = text.split(" - ")
+    bottexts = bottexts.split(", ")
+    nobita.insert_one({"usertext": usertext, "bottexts": bottexts})
+    await message.reply("Data saved!")
+    
+    
+    
+@app.on_message(
+    filters.command("del")
+    & filters.user(OWNER_ID)
+)
+async def del_text(client, message):
+    # Delete text from MongoDB
+    text = message.text.split(" ", 1)[1]
+    data = nobita.find_one({"usertext": text})
+    if data:
+        reply_text = ", ".join(data['bottexts'])
+        await message.reply(f"delete text : '{text}' and reply: '{reply_text}'")
+        nobita.delete_one({"usertext": text})
+        # Delete bot text from MongoDB
+        for bot_text in data['bottexts']:
+            Nobita.delete_one({"bottext": bot_text})
+        await message.reply(f"save text '{text}' and reply '{reply_text}' reply text deleted !")
+    else:
+        await message.reply("data no found!")
+        
+        
