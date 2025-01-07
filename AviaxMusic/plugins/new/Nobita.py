da5623739122675049336d8ad53a13e0a2f20581
Anjlnobita <jiteshkumar7200@gmail.com>
2025-01-07 16:25:36 +0530

    Update Nobita.py

--- AviaxMusic/plugins/new/Nobita.py
+++ AviaxMusic/plugins/new/Nobita.py
@@ -24,18 +24,20 @@ async def tsave(client, message):
     
     
     
+
 @app.on_message(filters.command("del") & filters.user(OWNER_ID))
 async def del_text(client, message):
     # Delete text from MongoDB
     text = message.text.split(" ", 1)[1]
+    usertext = text  # Add this line to define usertext
     data = await nobita.find_one({"usertext": usertext})
     if data:
         bottexts = ", ".join(data['bottexts'])
         await message.reply(f"Delete text: '{usertext}' and reply: '{bottexts}'")
         await nobita.delete_one({"usertext": usertext})
         # Delete bot text from MongoDB
-        for bottexts in data['bottexts']:
-            await nobita.delete_one({"bottexts": bottexts})
+        for bot_text in data['bottexts']:
+            await nobita.delete_one({"bottext": bot_text})
         await message.reply(f"Save text '{usertext}' and reply '{bottexts}' reply text deleted!")
     else:
         await message.reply("Data not found!")
