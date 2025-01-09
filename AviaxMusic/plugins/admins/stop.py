91498bf73459432ac02172c7bce10e90ca6037d2
Anjlnobita <jiteshkumar7200@gmail.com>
2025-01-10 04:40:46 +0530

    Update stop.py

--- AviaxMusic/plugins/admins/stop.py
+++ AviaxMusic/plugins/admins/stop.py
@@ -1,27 +1,22 @@
-from pyrogram import filters, Client
+from pyrogram import filters
 from pyrogram.types import Message
 
-from RISHUMUSIC import app
-from RISHUMUSIC.core.call import RISHU
-from RISHUMUSIC.utils.database import set_loop
-from RISHUMUSIC.utils.decorators import AdminRightsCheck
-from RISHUMUSIC.utils.inline import close_markup
+from AviaxMusic import app
+from AviaxMusic.core.call import Aviax
+from AviaxMusic.utils.database import set_loop
+from AviaxMusic.utils.decorators import AdminRightsCheck
+from AviaxMusic.utils.inline import close_markup
 from config import BANNED_USERS
 
 
-@Client.on_message(
-    filters.command(
-        ["end", "stop", "cend", "cstop"],
-        prefixes=["/", "!", "%", ",", "", ".", "@", "#"],
-    )
-    & filters.group
-    & ~BANNED_USERS
+@app.on_message(
+    filters.command(["end", "stop", "cend", "cstop"]) & filters.group & ~BANNED_USERS
 )
 @AdminRightsCheck
 async def stop_music(cli, message: Message, _, chat_id):
     if not len(message.command) == 1:
         return
-    await RISHU.stop_stream(chat_id)
+    await Aviax.stop_stream(chat_id)
     await set_loop(chat_id, 0)
     await message.reply_text(
         _["admin_5"].format(message.from_user.mention), reply_markup=close_markup(_)
